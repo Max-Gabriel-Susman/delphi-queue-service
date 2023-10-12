cc17dbc2cd020872fb205992c81bf0c8c45c18fd
Gabe Susman <gabe.susman@gmail.com>
2023-10-12 16:12:43 -0600

    played around with some poc work

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type Command struct {
+	Name string
+	Args []string
+}
+
 func main() {
-	println("We do it live!")
+	cmdA := Command{
+		Name: "eksctl",
+		Args: []string{"create", "cluster", "--name", "delphi-cluster", "--region", "us-east-1"},
+	}
+	cmdB := Command{
+		Name: "ls",
+		Args: []string{"-l"},
+	}
+
+	out, err := ExecuteCommand(cmdB.Name, cmdB.Args...)
+	fmt.Println("out: ", out)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	out, err = ExecuteCommand(cmdA.Name, cmdA.Args...)
+	fmt.Println("out: ", out)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+}
+
+func ExecuteCommand(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println("command execution failed: ", err)
+		// panic(err)
+	}
+
+	fmt.Print(strings.TrimSpace(string(stdout)))
+	return strings.TrimSpace(string(stdout)), nil
 }
